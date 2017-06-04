850f8c3042903ad6425b142b2efdb5b1b8fd6436
windows <352829638@qq.com>
2017-06-04 16:04:22 +0800

    325

--- mywx.php
+++ mywx.php
@@ -86,7 +86,7 @@ class wechatCallbackapiTest
                             $msgType = "text";
 		                	$contentStr = "欢迎来到微信的世界!";
 		                	$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-		                	$database->insert('text',['user'=>$fromUsername,'text'=>$keyword]);
+		                	//$database->insert('text',['user'=>$fromUsername,'text'=>$keyword]);
 		                	echo $resultStr;
                  	break;
                  }
