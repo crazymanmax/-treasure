bd18768f05b6c2a203282bc44ecbb11ad19d61f4
windows <352829638@qq.com>
2017-06-05 11:38:14 +0800

    ktktktk

--- mywx.php
+++ mywx.php
@@ -13,7 +13,8 @@ if ($_GET['echostr'])
 }
 else
 {
-	$wechatObj->responseMsg();
+	//$wechatObj->responseMsg();
+	echo $wechatObj->getAccessToken();
 }
 
 //$wechatObj->responseMsg();
@@ -294,6 +295,64 @@ class wechatCallbackapiTest
 			return false;
 		}
 	}
+
+    /*
+     * curl请求，获取返回的数据
+     * */
+    public function getData($url)
+    {
+        // 1. cURL初始化
+        $ch = curl_init();
+
+        // 2. 设置cURL选项
+        /*
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        */
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+        // 3. 执行cURL请求
+        $ret = curl_exec($ch);
+
+        // 4. 关闭资源
+        curl_close($ch);
+
+        return $ret;
+    }
+
+    /*
+     * JSON 转化为数组
+     * */
+    public function jsonToArray($json)
+    {
+        $arr = json_decode($json, 1);
+        return $arr;
+    }
+
+    public function getAccessToken(){
+        // redis  memcache SESSION
+        session_start();
+
+        if ($_SESSION['access_token'] && (time()-$_SESSION['expire_time']) < 7000 )
+        {
+            return $_SESSION['access_token'];
+        } else {
+            $appid = "wx58f393b77aeb9cf1";
+            $appsecret = "fba09072347681949aae45301c8c4de5";
+
+            $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$appid."&secret=".$appsecret;
+            $access_token = $this->jsonToArray($this->getData($url))['access_token'];
+
+            // 写入SESSION
+            $_SESSION['access_token'] = $access_token;
+            $_SESSION['expire_time'] = time();
+            return $access_token;
+        }
+    }
+
 }
 
 ?>
