41e100502ba971ad249d37d4a7948f02b35ff3cc
windows <352829638@qq.com>
2017-06-05 20:44:06 +0800

    skdere

--- catlist.php
+++ catlist.php
@@ -1,8 +1,67 @@
 <?php
 
     //下拉菜单
-   require('./mywx.php');
-   $wechatObj = new wechatCallbackapiTest();
+   
+class getData1{
+
+    public function getData($url,$data=null)
+    {
+        // 1. cURL初始化
+        $ch = curl_init();
+
+        // 2. 设置cURL选项
+        /*
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        */
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+        if(!empty($data)){
+        	curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        }
+
+        // 3. 执行cURL请求
+        $ret = curl_exec($ch);
+
+        // 4. 关闭资源
+        curl_close($ch);
+
+        return $ret;
+    }
+
+     public function jsonToArray($json)
+    {
+        $arr = json_decode($json, 1);
+        return $arr;
+    }
+
+    public function getAccessToken()
+    {
+        // redis  memcache SESSION
+        session_start();
+
+        if ($_SESSION['access_token'] && (time()-$_SESSION['expire_time']) < 7000 )
+        {
+            return $_SESSION['access_token'];
+        } else {
+            $appid = "wx58f393b77aeb9cf1";
+            $appsecret = "fba09072347681949aae45301c8c4de5";
+
+            $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$appid."&secret=".$appsecret;
+            $access_token = $this->jsonToArray($this->getData($url))['access_token'];
+
+            // 写入SESSION
+            $_SESSION['access_token'] = $access_token;
+            $_SESSION['expire_time'] = time();
+            return $access_token;
+        }
+    }
+}
+
 
     $json='{
 		     "button":[
@@ -33,10 +92,13 @@
 		            }]
 		       }]
 		 }';
-       
+  
+
+       $mod=new getData1;
+
         //获取access_token的值
-        $id=$wechatObj->getAccessToken(); 
+        $id=$mod->getAccessToken(); 
 
         $url="https://api.weixin.qq.com/cgi-bin/menu/create?access_token={$id}";
-        $wechatObj->getData($url,$json);
+        $mod->getData($url,$json);
 		 
