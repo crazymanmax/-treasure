0d404adfb5362a8a47f0f979aef04ef0fafcaf56
windows <352829638@qq.com>
2017-06-06 21:10:57 +0800

    defiewss

--- mywx.php
+++ mywx.php
@@ -242,6 +242,7 @@ class wechatCallbackapiTest
                            $url="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx58f393b77aeb9cf1&redirect_uri=".urlencode('http://39.108.1.202/weixin/bb.php')."&response_type=
 code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
                             header('location:'.$url);
+                            exit;
                  	break;
 
                  	default:
